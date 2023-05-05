3fb8fb14748b8da42cdaba4ea5856a2d4fb74995
Elliot Plant <elliotaplant@gmail.com>
2023-05-05 14:32:53 -0700

    Remove .envrc

--- netlify/functions/generate-clue.ts
+++ netlify/functions/generate-clue.ts
@@ -1,25 +1,5 @@
 import { Handler } from '@netlify/functions';
-
-function locationOnlyPrompt(location) {
-  return `Act as a scavenger hunt clue generator. 
-Create rhyming riddle clues to indicate where the next clue is located. 
-Your response should have four short lines.
-Do not include words from the Location in the riddle.
-
-Location: ${location}`;
-}
-
-function locationAndContextPrompt(location, context) {
-  return `Act as a scavenger hunt clue generator. 
-Create rhyming riddle clues to indicate where the next clue is located. 
-Your response should have four short lines. 
-Do not include words from the Location in the riddle.
-Include the context in the riddle if possible. 
-
-Location: ${location}
-
-Context: ${context}`;
-}
+import generateClue from '../../defer/generateClue';
 
 export const handler: Handler = async (event) => {
   if (event.httpMethod !== 'GET') {
@@ -33,40 +13,9 @@ export const handler: Handler = async (event) => {
     return { statusCode: 400, body: "Missing required 'location' parameter" };
   }
 
-  const prompt = contextParam
-    ? locationAndContextPrompt(locationParam, contextParam)
-    : locationOnlyPrompt(locationParam);
+  const clueId = String(Math.random() * 1e18);
 
-  console.log('prompt', prompt);
+  await generateClue(clueId, locationParam, contextParam);
 
-  const clue = await Promise.race([getCompletion(prompt), timeoutTimer(8000)]);
-
-  return { statusCode: 200, body: JSON.stringify({ clue }) };
+  return { statusCode: 202, body: JSON.stringify({ clueId }) };
 };
-
-async function getCompletion(prompt) {
-  const response = await fetch('https://api.openai.com/v1/chat/completions', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-      'Authorization': `Bearer ${process.env.OPENAI_API_KEY}`,
-    },
-    body: JSON.stringify({
-      model: 'gpt-3.5-turbo',
-      messages: [{ role: 'user', content: prompt }],
-      max_tokens: 200,
-      n: 1,
-      stop: null,
-      temperature: 0.7,
-    }),
-  });
-
-  const data = await response.json();
-  const clue = data?.choices?.at(0)?.message?.content?.trim();
-  return clue;
-}
-
-async function timeoutTimer(ms) {
-  await new Promise((resolve) => setTimeout(resolve, ms));
-  return 'Timeout';
-}
