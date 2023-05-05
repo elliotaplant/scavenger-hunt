f2a958f2a2fdccc60fa32183fa382805fb2ad945
Elliot Plant <elliotaplant@gmail.com>
2023-05-05 14:39:04 -0700

    Remove try defer stuff

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,19 +1,8 @@
 export function Header() {
-  const tryDefer = async () => {
-    try {
-      const response = await fetch('/.netlify/functions/try-defer');
-      const body = await response.json();
-      console.log('body', body);
-    } catch (error: any) {
-      console.error(error);
-    }
-  };
-
   return (
     <header>
       <h1>Scavenger Hunt Generator</h1>
       <p>Generate a scavenger hunt with AI!</p>
-      <button onClick={tryDefer}>Try Defer</button>
     </header>
   );
 }
